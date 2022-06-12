2acd2c8df8de9ebd7507c69ba53bec29fd258c05
Jimbo <fong.milk@gmail.com>
2022-06-12 20:04:24 +0800

    deploy contract

--- f2e/contract/ProposalFactory.js
+++ f2e/contract/ProposalFactory.js
@@ -4,7 +4,7 @@ import web3 from './web3'
 // hazel github
 import ProposalFactory from './json/proposalFactory.json'
 const ProposalFactoryABI = ProposalFactory.abi
-const ProposalFactoryAddress = '0x5843165711e0144f6c19c73aDAf78050Ed226Db3'
+const ProposalFactoryAddress = '0xD38024156CEDdf6c73bC9787777Eb982e7C0edEB'
 
 const instance = new web3.eth.Contract(
   ProposalFactoryABI,
