08d354873f48b20628533b2dc74aa73ce399a60a
Hazel Wu <tinazx056@gmail.com>
2022-06-12 17:18:33 +0800

    feat(Toast): create Toast caption

--- f2e/components/Toast.js
+++ f2e/components/Toast.js
@@ -1,7 +1,20 @@
 import { useToast } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
+/*
+  使用方式
+  1. 引入
+  import { useToastHook } from '/components/Toast'
 
-// status type: "info" | "warning" | "success" | "error" | "loading"
+  2. 設定
+  const [state, newToast] = useToastHook();
+
+  3. 呼叫
+  newToast({ message: '感謝贊助 🙏', status: "success" });
+
+  status type:
+  "info" | "warning" | "success" | "error" | "loading"
+*/
+
 export function useToastHook() {
   const [state, setState] = useState(undefined);
   const toast = useToast();
