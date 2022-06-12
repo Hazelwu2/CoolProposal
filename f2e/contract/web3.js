f8bac71fcd10b1ee8a9f49a54bb9c6dbf75fc7ef
Hazel Wu <tinazx056@gmail.com>
2022-06-12 13:32:52 +0800

    remove: console

--- f2e/contract/web3.js
+++ f2e/contract/web3.js
@@ -5,7 +5,6 @@ if (typeof window !== "undefined"
   && typeof window.web3 !== "undefined") {
   // we are in the browser and meta mask is installed
   console.error(1)
-  console.log(Web3.givenProvider)
   // web3 = new Web3(window.ethereum);
   web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
   // 您連接的節點沒有正確響應
@@ -20,6 +19,5 @@ if (typeof window !== "undefined"
   web3 = new Web3(provider);
 }
 
-console.log('web3', web3)
 
 export default web3
