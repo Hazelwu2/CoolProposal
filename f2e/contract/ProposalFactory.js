c69890e3e1cd587df4ddd600709184126fd84c4f
Hazel Wu <tinazx056@gmail.com>
2022-06-15 01:52:51 +0800

    feat(Contract): Update contract address & abi

--- f2e/contract/ProposalFactory.js
+++ f2e/contract/ProposalFactory.js
@@ -4,7 +4,7 @@ import web3 from './web3'
 // hazel github
 import ProposalFactory from './json/proposalFactory.json'
 const ProposalFactoryABI = ProposalFactory.abi
-const ProposalFactoryAddress = '0xA9854B17B6e65A72a334eB0f797786f7aC53Ebcc'
+const ProposalFactoryAddress = '0xd4B2F2D48bAaBc42996BC71ff30E10b4A77f4740'
 
 const instance = new web3.eth.Contract(
   ProposalFactoryABI,
