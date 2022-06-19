fcf7fe6b34ec7921b5eebf131fc3e079498eea70
Hazel Wu <tinazx056@gmail.com>
2022-06-19 23:12:55 +0800

    feat: 更改合約地址

--- f2e/contract/ProposalFactory.js
+++ f2e/contract/ProposalFactory.js
@@ -4,7 +4,8 @@ import web3 from './web3'
 // hazel github
 import ProposalFactory from './json/proposalFactory.json'
 const ProposalFactoryABI = ProposalFactory.abi
-const ProposalFactoryAddress = '0x5D242d68ae938346Af56650fEA43169e317f7F96'
+// const ProposalFactoryAddress = '0x5D242d68ae938346Af56650fEA43169e317f7F96'
+const ProposalFactoryAddress = '0x78e6816b82C8E41fB37DE53E99449acD66C54BE8'
 
 const instance = new web3.eth.Contract(
   ProposalFactoryABI,
