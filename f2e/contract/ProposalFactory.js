c2fe285c9b33f0ccc7f708394ee869fb22e904cc
Hazel Wu <tinazx056@gmail.com>
2022-06-19 23:50:28 +0800

    test

--- f2e/contract/ProposalFactory.js
+++ f2e/contract/ProposalFactory.js
@@ -1,8 +1,8 @@
 import web3 from './web3'
 // batch3
-// import ProposalFactory from '../../artifacts/contracts/Proposal.sol/ProposalFactory.json'
+import ProposalFactory from '../../artifacts/contracts/Proposal.sol/ProposalFactory.json'
 // hazel github
-import ProposalFactory from './json/proposalFactory.json'
+// import ProposalFactory from './json/proposalFactory.json'
 const ProposalFactoryABI = ProposalFactory.abi
 // const ProposalFactoryAddress = '0x5D242d68ae938346Af56650fEA43169e317f7F96'
 const ProposalFactoryAddress = '0x78e6816b82C8E41fB37DE53E99449acD66C54BE8'
